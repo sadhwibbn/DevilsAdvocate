3ecebca242595a087b2d24434731482c84817f64
Sadhwi <sadhwi@gmail.com>
2015-09-09 14:25:36 +0530

    New source files. Basic test - app dev has not yet begun.

---
+++ app/src/main/java/com/example/example_09072015/TestSqlConnection.java
@@ -0,0 +1,42 @@
+package com.example.example_09072015;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by Srinivas on 9/9/2015.
+ */
+public class TestSqlConnection
+{
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+            Connection conn = DriverManager.getConnection(url, "postgres", "poocha");
+
+            String sql = "SELECT * FROM \"Users\"";
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while(rs.next())
+            {
+                System.out.println(rs.getString("Email"));
+            }
+
+            st.close();
+            rs.close();
+            conn.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+}
